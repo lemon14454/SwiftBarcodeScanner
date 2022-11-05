0cf7f297ceacbbd6c76aac7c3d86d4b37c6108c4
lemon14454 <lemon14454@gmail.com>
2022-11-06 01:02:28 +0800

    Initial Commit

--- BarcodeScanner/BarcodeScannerApp.swift
+++ BarcodeScanner/BarcodeScannerApp.swift
@@ -1,17 +1,10 @@
-//
-//  BarcodeScannerApp.swift
-//  BarcodeScanner
-//
-//  Created by tom on 2022/11/5.
-//
-
 import SwiftUI
 
 @main
 struct BarcodeScannerApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            BarcodeScannerView()
         }
     }
 }
---
+++ BarcodeScanner/BarcodeScannerView.swift
@@ -0,0 +1,33 @@
+import SwiftUI
+
+struct BarcodeScannerView: View {
+    
+    @State private var scannedCode = ""
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                ScannerView(scannedCode: $scannedCode)
+                    .frame(maxWidth: .infinity, maxHeight: 300)
+                
+                Spacer().frame(height: 60)
+                    
+                Label("Scanned Barcode:", systemImage: "barcode.viewfinder")
+                    .font(.title)
+                
+                Text(scannedCode.isEmpty ? "Not Yet Scanned" : scannedCode)
+                    .bold()
+                    .font(.largeTitle)
+                    .foregroundColor(scannedCode.isEmpty ? .red : .green)
+                    .padding()
+            }
+            .navigationTitle("Barcode Scanner")
+        }
+    }
+}
+
+struct BarcodeScannerView_Previews: PreviewProvider {
+    static var previews: some View {
+        BarcodeScannerView()
+    }
+}
